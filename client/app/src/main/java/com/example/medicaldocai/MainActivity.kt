0690c49803dbf23b0a10d2a7cbad92bccc91bdd7
SurytheGreat <24je0705@iitism.ac.in>
2025-12-01 11:57:06 +0530

    basic frontend setup

---
+++ client/app/src/main/java/com/example/medicaldocai/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.medicaldocai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.medicaldocai.ui.screen.ChatViewModel
+import com.example.medicaldocai.ui.screen.MedicalChatScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MedicalChatScreen(viewModel = ChatViewModel())
+        }
+    }
+}
